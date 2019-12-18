165ee86bfd7598ec97d28a48b5f4cb85842f9e6b
baztian <bastian.dev@gmail.com>
2019-12-18 23:01:05 +0100

    Include README for pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 import setuptools
 from distutils.core import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'rfid-squeezectl',
   py_modules=['rfid_squeezectl'],
   version = '0.1.0',
   license='Apache',
   description = 'Control Logitech Squeezebox using RFID tags',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   author = 'Bastian Bowe',
   author_email = 'bastian.dev@gmail.com',
   url = 'https://github.com/baztian/rfid-squeezectl',
